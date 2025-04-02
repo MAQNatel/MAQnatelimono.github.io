3628fc58e78ec2edc328b75113196b8d6eed5d84
MAQNatelimono <v-nlimono@microsoft.com>
2025-04-02 10:29:44 -0700

    Update sidenav.js
    Signed-off-by: MAQNatelimono <v-nlimono@microsoft.com>

--- js/sidenav.js
+++ js/sidenav.js
@@ -11,6 +11,20 @@ $("#header").load("/header", function() {
     });
 });
 
+function headerConstructor() {
+    $("#header").load("/header", function() {
+        $('#header .icon-menu-hamburger').on('click', function() {
+          // console.log('Nav item clicked in header!');
+            const navbar = document.getElementById('navbar-right');
+            navbar.classList.toggle('closed');
+        });
+        $('#header .dismiss-button').on('click', function() {
+          // console.log('Dismiss clicked!');
+            const navbar = document.getElementById('navbar-right');
+            navbar.classList.toggle('closed');
+        });
+    });
+}
 // const hamburger = document.getElementById('icon-menu-hamburger');
 // const navbar = document.getElementById('navbar');
 
